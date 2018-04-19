e070dab236aac11826a83daad325c69fa410ef1e
Raviteja Medavaram <raviteja.medavaram@gmail.com>
2018-04-19 19:34:16 +0530

    Ravi

--- src/main/resources/static/js/base.js
+++ src/main/resources/static/js/base.js
@@ -1,23 +1,41 @@
-
-var request = new XMLHttpRequest();
-
-
 function makeCall() {
+    var request = new XMLHttpRequest();
+
     var name = document.getElementById("birthdayBoy").value;
     var dob = document.getElementById("birthdayDate").value;
     var auto_wish = document.getElementById("auto_wish").checked;
     // var request = new XMLHttpRequest();
     request.open('POST', '/add', true);
-    request.setRequestHeader("Content-type","application/json");
-    request.send(JSON.stringify({dob:dob,name:name,auto_wish:auto_wish}));
+    request.setRequestHeader("Content-type", "application/json");
+    request.send(JSON.stringify({dob: dob, name: name, auto_wish: auto_wish}));
 }
 
 function viewAllBirthdays() {
+    var request = new XMLHttpRequest();
+    var response = null;
 
-    request.open('GET','/viewAll',true);
-    request.setRequestHeader("Content-type","application/json");
+    request.open('GET', '/getAllBirthdays', true);
+    request.setRequestHeader("Content-type", "application/json");
     request.send();
-    for (var row in this.response){
+    request.onreadystatechange = function () {
+        if (request.readyState == XMLHttpRequest.DONE) {
+            alert(request.response);
+        }
+        response = JSON.parse(request.responseText);
+    };
+    var keys =response[0];
 
+
+
+    var divContainer = document.getElementById("BirthdayList");
+    var table = divContainer.createElement("TABLE");
+    // var headers = table.appendChild("TH");
+    for(name in keys){
+        // console.log(name)
+        table.appendChild("TH");
     }
+
+    divContainer.innerHTML = "";
+    divContainer.appendChild(table);
+
 }
